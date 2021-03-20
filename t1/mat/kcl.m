c2b661e1414474ae0aa800506f8fda6236c818b5
Ana Martins <anaisabelmartins@tecnico.ulisboa.pt>
2021-03-20 23:03:08 +0000

    changes to kcl and kvl

--- t1/mat/kcl.m
+++ t1/mat/kcl.m
@@ -15,22 +15,23 @@ syms V7
 syms Vb
 syms Vc
 
-R1 = 1.02779694281e-3 %kOhm
-R2 = 2.07715672461e-3%kOhm tyyy
-R3 = 3.1156225241e-3%kOhm
-R4 = 4.11105908751e-3%kOhm
-R5 = 3.02879032081e-3%kOhm
-R6 = 2.02983164661e-3 %kOhm
-R7 = 1.00454956611e-3 %kOhm
-Va = 5.18504190779 %V
-Id = 1.03147590492e2 %mA
-Kb = 7.00248816446e2 %mS
-Kc = 8.14562893937e-3 %kOhm
+zero = 0.0
+one = 1.0
 
-a = [1/R1, -(R1+R2)/(R1*R2), 1/R2, 0, 0, 0, 0, -1/R3, 0; 0, 1/R2, -1/R2, 0, 0, 0, 0, Kb, 0; 0, 0, 0, 0, 1/R5, -1/R5, 0, -Kb, 0; 0, 0, 0, 1/R6, 0, 0, -(R6+R7)/(R6*R7), 0, 0; 1, 0, 0, -1, 0, 0, 0, 0, 0; 0, 1, 0, 0, -1, 0, 0 -1, 0; 0, 0, 0, 0, 1, 0, 0, 0, -1; 0, 0, 0, -1/R6, 0, 0, 1/R6, 0, 1/Kc; 0, 0, 0, 0, 0, 0, -1/R7, 0, Kc]
+R1 = 1.02779694281e3 %kOhm
+R2 = 2.07715672461e3%kOhm tyyy
+R3 = 3.1156225241e3%kOhm
+R4 = 4.11105908751e3%kOhm
+R5 = 3.02879032081e3%kOhm
+R6 = 2.02983164661e3 %kOhm
+R7 = 1.00454956611e3 %kOhm
+Va = 5.18504190779 %V
+Id = 1.03147590492e-3 %mA
+Kb = 7.00248816446e-3 %mS
+Kc = 8.14562893937e3 %kOhm
+
+a = [one/R1, -(R1+R2)/(R1*R2), one/R2, zero, zero, zero, zero, -one/R3, zero; zero, one/R2, -one/R2, zero, zero, zero, zero, Kb, zero; zero, zero, zero, zero, one/R5, -one/R5, zero, -Kb, zero; zero, zero, zero, one/R6, zero, zero, -(R6+R7)/(R6*R7), zero, zero; one, zero, zero, -one, zero, zero, zero, zero, zero; zero, one, zero, zero, -one, zero, zero -one, zero; zero, zero, zero, zero, one, zero, zero, zero, -one; zero, zero, zero, -one/R6, zero, zero, one/R6, zero, one/Kc; zero, zero, zero, zero, zero, zero, -one/R7, zero, Kc]
 x = [V1; V2; V3; V4; V5; V6; V7; Vb; Vc]
-b = [0; 0; -Id; 0; Va; 0; 0; 0; 0]
+b = [zero; zero; -Id; zero; Va; zero; zero; zero; zero]
 
 x = a\b
-
-V1 = x(1)
--- t1/mat/kvl.m
+++ t1/mat/kvl.m
@@ -12,21 +12,24 @@ syms I4
 syms Ib
 syms Ic
 
-R1 = 1.02779694281e-3 %kOhm
-R2 = 2.07715672461e-3%kOhm tyyy
-R3 = 3.1156225241e-3%kOhm
-R4 = 4.11105908751e-3%kOhm
-R5 = 3.02879032081e-3%kOhm
-R6 = 2.02983164661e-3 %kOhm
-R7 = 1.00454956611e-3 %kOhm
-Va = 5.18504190779 %V
-Id = 1.03147590492e2 %mA
-Kb = 7.00248816446e2 %mS
-Kc = 8.14562893937e-3 %kOhm
+zero = 0.0
+one = 1.0
 
-a = [R1+R4, 0, -R4, 0, 1/Kb, 0; 0, 1, 0, 0, 1, 0; -R4, 0, (R4+R6+R7), 0, 0, Kc; 0, 0, 0, 1, 0, 0; 0, 0, 1, 0, 0, 1; -R3, R3, 0, 0, 1/Kb, 0, 0]
+R1 = 1.02779694281e3 %kOhm
+R2 = 2.07715672461e3%kOhm tyyy
+R3 = 3.1156225241e3%kOhm
+R4 = 4.11105908751e3%kOhm
+R5 = 3.02879032081e3%kOhm
+R6 = 2.02983164661e3 %kOhm
+R7 = 1.00454956611e3 %kOhm
+Va = 5.18504190779 %V
+Id = 1.03147590492e-3 %mA
+Kb = 7.00248816446e-3 %mS
+Kc = 8.14562893937e3 %kOhm
+
+a = [(R1+R4), zero, -R4, zero, one/Kb, zero; zero, one, zero, zero, one, zero; -R4, zero, (R4+R6+R7), zero, zero, Kc; zero, zero, zero, one, zero, zero; zero, zero, one, zero, zero, one; -R3, R3, zero, zero, one/Kb, zero]
 x = [I1; I2; I3; I4; Ib; Ic]
-b = [Va; 0; 0; -Id; 0; 0]
+b = [Va; zero; zero; -Id; zero; zero]
 
 x = a\b
 disp x
