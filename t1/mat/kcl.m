cefcd9e06fa6eb3a65805728529841f7d407b2c8
Ana Martins <anaisabelmartins@tecnico.ulisboa.pt>
2021-03-21 13:34:30 +0000

    changes to kcl and kvl

--- t1/mat/kcl.m
+++ t1/mat/kcl.m
@@ -1,8 +1,6 @@
 close all
 clear all
 
-%%EXAMPLE SYMBOLIC COMPUTATIONS
-
 pkg load symbolic
 
 syms V1
@@ -15,23 +13,23 @@ syms V7
 syms Vb
 syms Vc
 
-zero = 0.0
-one = 1.0
+Z = vpa(0.0)
+O = vpa(1.0)
 
-R1 = 1.02779694281e3 %kOhm
-R2 = 2.07715672461e3%kOhm tyyy
-R3 = 3.1156225241e3%kOhm
-R4 = 4.11105908751e3%kOhm
-R5 = 3.02879032081e3%kOhm
-R6 = 2.02983164661e3 %kOhm
-R7 = 1.00454956611e3 %kOhm
-Va = 5.18504190779 %V
-Id = 1.03147590492e-3 %mA
-Kb = 7.00248816446e-3 %mS
-Kc = 8.14562893937e3 %kOhm
+R1 = vpa(1.02779694281e3) %Ohm
+R2 = vpa(2.07715672461e3) %Ohm
+R3 = vpa(3.1156225241e3) %Ohm
+R4 = vpa(4.11105908751e3) %Ohm
+R5 = vpa(3.02879032081e3) %Ohm
+R6 = vpa(2.02983164661e3) %Ohm
+R7 = vpa(1.00454956611e3) %Ohm
+Va = vpa(5.18504190779) %V
+Id = vpa(1.03147590492e-3) %A
+Kb = vpa(7.00248816446e-3) %S
+Kc = vpa(8.14562893937e3) %Ohm
 
-a = [one/R1, -(R1+R2)/(R1*R2), one/R2, zero, zero, zero, zero, -one/R3, zero; zero, one/R2, -one/R2, zero, zero, zero, zero, Kb, zero; zero, zero, zero, zero, one/R5, -one/R5, zero, -Kb, zero; zero, zero, zero, one/R6, zero, zero, -(R6+R7)/(R6*R7), zero, zero; one, zero, zero, -one, zero, zero, zero, zero, zero; zero, one, zero, zero, -one, zero, zero -one, zero; zero, zero, zero, zero, one, zero, zero, zero, -one; zero, zero, zero, -one/R6, zero, zero, one/R6, zero, one/Kc; zero, zero, zero, zero, zero, zero, -one/R7, zero, Kc]
+a = [O/R1, -(R1*R2+R1*R3+R2*R3)/(R1*R2*R3), O/R2, Z, O/R3, Z, Z, Z, Z; Z, O/R2, -O/R2, Z, Z, Z, Z, Kb, Z; Z, Z, Z, Z, O/R5, -O/R5, Z, -Kb, Z; Z, Z, Z, O/R6, Z, Z, -(R6+R7)/(R6*R7), Z, Z; O, Z, Z, -O, Z, Z, Z, Z, Z; Z, O, Z, Z, -O, Z, Z -O, Z; Z, Z, Z, Z, O, Z, Z, Z, -O; Z, Z, Z, -O/R6, Z, Z, O/R6, Z, O/Kc; Z, Z, Z, Z, Z, Z, -O/R7, Z, Kc]
 x = [V1; V2; V3; V4; V5; V6; V7; Vb; Vc]
-b = [zero; zero; -Id; zero; Va; zero; zero; zero; zero]
+b = [Z; Z; -Id; Z; Va; Z; Z; Z; Z]
 
 x = a\b
--- t1/mat/kvl.m
+++ t1/mat/kvl.m
@@ -12,24 +12,23 @@ syms I4
 syms Ib
 syms Ic
 
-zero = 0.0
-one = 1.0
+Z = vpa(0.0)
+O = vpa(1.0)
 
-R1 = 1.02779694281e3 %kOhm
-R2 = 2.07715672461e3%kOhm tyyy
-R3 = 3.1156225241e3%kOhm
-R4 = 4.11105908751e3%kOhm
-R5 = 3.02879032081e3%kOhm
-R6 = 2.02983164661e3 %kOhm
-R7 = 1.00454956611e3 %kOhm
-Va = 5.18504190779 %V
-Id = 1.03147590492e-3 %mA
-Kb = 7.00248816446e-3 %mS
-Kc = 8.14562893937e3 %kOhm
+R1 = vpa(1.02779694281e3) %Ohm
+R2 = vpa(2.07715672461e3) %Ohm
+R3 = vpa(3.1156225241e3) %Ohm
+R4 = vpa(4.11105908751e3) %Ohm
+R5 = vpa(3.02879032081e3) %Ohm
+R6 = vpa(2.02983164661e3) %Ohm
+R7 = vpa(1.00454956611e3) %Ohm
+Va = vpa(5.18504190779) %V
+Id = vpa(1.03147590492e-3) %A
+Kb = vpa(7.00248816446e-3) %S
+Kc = vpa(8.14562893937e3) %Ohm
 
-a = [(R1+R4), zero, -R4, zero, one/Kb, zero; zero, one, zero, zero, one, zero; -R4, zero, (R4+R6+R7), zero, zero, Kc; zero, zero, zero, one, zero, zero; zero, zero, one, zero, zero, one; -R3, R3, zero, zero, one/Kb, zero]
-x = [I1; I2; I3; I4; Ib; Ic]
-b = [Va; zero; zero; -Id; zero; zero]
+A = [(R1+R4), Z, -R4, Z, O/Kb, Z; Z, O, Z, Z, O, Z; -R4, Z, (R4+R6+R7), Z, Z, Kc; Z, Z, Z, O, Z, Z; Z, Z, O, Z, Z, O; -R3, R3, Z, Z, O/Kb, Z]
+B = [Va; Z; Z; -Id; Z; Z]
+X = [I1; I2; I3; I4; Ib; Ic]
 
-x = a\b
-disp x
+X = A\B
