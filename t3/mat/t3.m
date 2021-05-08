ec549d7deadc9d655e7c9386f834e1131b4c8668
MiguelMoreira96556 <migueljosemoreira@tecnico.ulisboa.pt>
2021-05-08 14:14:53 +0100

    octave update

--- t3/mat/t3.m
+++ t3/mat/t3.m
@@ -12,7 +12,7 @@ Rr = 300e3; % Ohm R regulator
 n = 0.153;
 nd = 20.; % numero de diodos
 
-t = 0:1e-5:200e-3;
+t = 5:1e-5:5.2; %para não começar no 0
 
 f = 50; %Hz
 w = 2 * pi * f; %rad.s⁻¹
@@ -27,7 +27,7 @@ Von = 12./nd; %vout/nº de díodos
 V7 = abs(V3); %ACHO EU (o burro do mike)
 
 toff = 1/w * atan(1/(w*Re*C));
-%para t>toff Vcondensador=A*cos(w*toff)*exp(^-((t-toff)/(R*C)))
+%para t>toff Vcondensador=A*cos(w*toff)*exp(^(-(t-toff)/(R*C)))
 
 eta = 1;
 V_T = 25e-3; %mV
@@ -37,20 +37,20 @@ Vd = 230; %V ACHO EU VEJAM AQUI
 rd = (nd * eta * V_T) / (I_S * exp(Vd / (nd * eta * Vd)));
 
 %envelope
-%time axis: 0 to 20ms with 1us steps
+%time axis: 5000ms to 5200ms with 1us steps
 A = 1; % acho eu
-% para t off
-v0 = A * cos(w*toff)* exp(-(t-toff)/(Re*C)); % usa-se Re aqui ou uma soma?
+% para t > toff
+v0 = A * cos(w*toff) * exp(-(t-toff)/(Re*C)); % usa-se Re aqui ou uma soma?
 v0f = figure();
 
 
 plot(t,v0)
 hold on
 
-axis([0, 20e-3, 0, 10]);
+xlim([5, 5.2]);
 xlabel("t[s]");
 ylabel("v0[V]");
 print (v0f, "mat-envelope.eps", "-depsc");
 
 
-Vripple = A * (1 - exp(-T/(Re*C))); % é com o R do envelope?
+Vripple = A * (1 - exp(-T/(2*Re*C))); % é com o R do envelope?
