530b9de21e2fe82d53d48f0374c642e7872248c6
MiguelMoreira96556 <migueljosemoreira@tecnico.ulisboa.pt>
2021-05-08 16:06:46 +0100

    octave update

--- t3/mat/t3.m
+++ t3/mat/t3.m
@@ -20,13 +20,16 @@ T = 1/f;
 
 Vin = 230 * cos(w * t); %V
 
-V3 = n * Vin;
+Vs = n * Vin;
+
+A = 230/n;
 
 Von = 12./nd; %vout/nº de díodos
+vlim = nd * Von;
 
-V7 = abs(V3); %ACHO EU (o burro do mike)
+V7 = abs(Vs); %ACHO EU (o burro do mike)
 
-toff = 1/w * atan(1/(w*Re*C));
+toff = 1/(w) * atan(1/(w*Re*C));
 %para t>toff Vcondensador=A*cos(w*toff)*exp(^(-(t-toff)/(R*C)))
 
 eta = 1;
@@ -38,14 +41,41 @@ rd = (nd * eta * V_T) / (I_S * exp(Vd / (nd * eta * Vd)));
 
 %envelope
 %time axis: 5000ms to 5200ms with 1us steps
-A = 1; % acho eu
+
 % para t > toff
-v0 = A * cos(w*toff) * exp(-(t-toff)/(Re*C)); % usa-se Re aqui ou uma soma?
+Vexp = A * cos(w*toff) * exp(-(t-toff)/(Re*C)); % usa-se Re aqui ou uma soma?
+Vcos = Vs;
 v0f = figure();
 
+printf("toff = %.10f", toff);
 
-plot(t,v0)
-hold on
+%equação para sacar ton
+function y = f(x);
+  w = 2 * pi * f; %rad.s⁻¹
+  %Re = 700e3; %Ohm R envelope
+  %C = 485e-6; %uFarad
+  %toff = 1/w * atan(1/(w*Re*C));
+  y = cos(w*x) - cos(w*toff)*exp(-(x-toff)/(Re*C));
+endfunction
+
+x0 = 0;
+%ton = fsolve("f", x0);
+
+i = 0;
+while(i < 10)
+  if(t > ((249+i)*toff) && t < ((249+i)*ton))
+    v0 = Vcos;
+  end
+  
+ if(t < ((249+i)*toff) && t >~ ((249+i)*ton))
+   v0 = Vexp;
+ end
+  
+  i = i + 1;
+end
+
+plot(t,v0);
+hold on;
 
 xlim([5, 5.2]);
 xlabel("t[s]");
@@ -53,4 +83,5 @@ ylabel("v0[V]");
 print (v0f, "mat-envelope.eps", "-depsc");
 
 
-Vripple = A * (1 - exp(-T/(2*Re*C))); % é com o R do envelope?
+%Vripple = A * (1 - exp(-T/(2*Re*C))); % é com o R do envelope? acho que isto não é preciso
+
