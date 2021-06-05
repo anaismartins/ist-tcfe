518b03f6dea3fb7ab567c914af0f797b8032f7da
MiguelMoreira96556 <migueljosemoreira@tecnico.ulisboa.pt>
2021-06-05 17:57:10 +0100

    octave update

--- t5/mat/t5.m
+++ t5/mat/t5.m
@@ -8,7 +8,7 @@ Cin1 = 1e-6; %F
 Cin2 = 1e-6; %F
 Cin3 = 1e-6; %F
 
-f = 1000; %Hz
+f = 2.383202e+03; %Hz
 omega = 2 * pi * f;
 
 Z1 = R1 + 1/(j * omega * Cin1);
@@ -20,8 +20,8 @@ Zeq = 1/(1/Ztemp + 1/Z3); %impedância ao nível de inv_in
 
 Vin = 0.01 * exp(j * pi / 2); %seno tem fase de pi/2
 
-gaincomp = R2/Zeq; %slide 2 aula 23
-gain = real(gaincomp);
+gaincomp = -R2/Zeq; %slide 2 aula 23
+gain = abs(gaincomp);
 gaindB = 20 * log10(gain);
 
 Zi = real(Zeq); %slide 2 aula 23
@@ -36,3 +36,32 @@ fprintf(file, "Zo & %.4e\\\\\\hline ", Zo);
 
 fflush(file);
 fclose(file);
+
+a = 1;
+
+for i = 1:0.1:8.1 %ou seja, 10 pontos por década
+  f = power(10, i);
+  omega = 2 * pi * f;
+  Z1 = R1 + 1/(j * omega * Cin1);
+  Z2 = R2 + 1/(j * omega * Cin2);
+  Z3 = R3 + 1/(j * omega * Cin3);
+  
+  Ztemp = 1/(1/Z1 + 1/Z2); %paralelo de Z1 com Z2
+  Zeq = 1/(1/Ztemp + 1/Z3); %impedância ao nível de inv_in
+  
+  gaincomp = R2/Zeq; %slide 2 aula 23
+  gain = abs(gaincomp);
+  gaindB(a) = 20 * log10(gain);
+  
+  a = a + 1;
+  
+endfor
+
+f = 1:0.1:8.1; %início, fim e 70 pontos pelo meio
+hf = figure();
+plot(f, gaindB);
+legend("Gain");
+xlabel("log(f)[Hz]");
+ylabel("Gain[dB]");
+
+print (hf, "gainteo.eps", "-depsc");
