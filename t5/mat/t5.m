28225a13fbde5bc0112404b3019164356f1426c8
MiguelMoreira96556 <migueljosemoreira@tecnico.ulisboa.pt>
2021-06-05 15:39:39 +0100

    octave1 feito (acho)

--- t5/mat/t5.m
+++ t5/mat/t5.m
@@ -9,6 +9,7 @@ Cin2 = 1e-6; %F
 Cin3 = 1e-6; %F
 
 f = 1000; %Hz
+omega = 2 * pi * f;
 
 Z1 = R1 + 1/(j * omega * Cin1);
 Z2 = R2 + 1/(j * omega * Cin2);
@@ -21,7 +22,17 @@ Vin = 0.01 * exp(j * pi / 2); %seno tem fase de pi/2
 
 gaincomp = R2/Zeq; %slide 2 aula 23
 gain = real(gaincomp);
+gaindB = 20 * log10(gain);
 
-Zi = Zeq; %slide 2 aula 23
+Zi = real(Zeq); %slide 2 aula 23
 Zo = 0; %estranho :/
 
+file = fopen("Teo1.tex", "w");
+
+fprintf(file, "gain & %.4e\\\\\\hline ", gain);
+fprintf(file, "gaindB & %.4e\\\\\\hline ", gaindB);
+fprintf(file, "Zi & %.4e\\\\\\hline ", Zi);
+fprintf(file, "Zo & %.4e\\\\\\hline ", Zo);
+
+fflush(file);
+fclose(file);
