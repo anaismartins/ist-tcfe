cdcb5b9d8757726a7ea17460f906933e42eec0f7
Ana Martins <anaisabelmartins@tecnico.ulisboa.pt>
2021-06-06 10:25:42 +0100

    added phase graph

--- t5/mat/t5.m
+++ t5/mat/t5.m
@@ -12,8 +12,8 @@ f = 2.383202e+03; %Hz
 omega = 2 * pi * f;
 
 Z1 = R1 + 1/(j * omega * Cin1);
-Z2 = R2 + 1/(j * omega * Cin2);
-Z3 = R3 + 1/(j * omega * Cin3);
+Z2 = R3 + 1/(j * omega * Cin2);
+Z3 = R4 + 1/(j * omega * Cin3);
 
 Ztemp = 1/(1/Z1 + 1/Z2); %paralelo de Z1 com Z2
 Zeq = 1/(1/Ztemp + 1/Z3); %impedância ao nível de inv_in
@@ -43,15 +43,19 @@ for i = 1:0.1:8.1 %ou seja, 10 pontos por década
   f = power(10, i);
   omega = 2 * pi * f;
   Z1 = R1 + 1/(j * omega * Cin1);
-  Z2 = R2 + 1/(j * omega * Cin2);
-  Z3 = R3 + 1/(j * omega * Cin3);
+  Z2 = R3 + 1/(j * omega * Cin2);
+  Z3 = R4 + 1/(j * omega * Cin3);
   
   Ztemp = 1/(1/Z1 + 1/Z2); %paralelo de Z1 com Z2
   Zeq = 1/(1/Ztemp + 1/Z3); %impedância ao nível de inv_in
+
+  Zeq2 = 1/(1/Z1 + 1/Z2 + 1/Z3) + R2; % usei para tentar calcular a fase mas está mal
   
   gaincomp = R2/Zeq; %slide 2 aula 23
   gain = abs(gaincomp);
   gaindB(a) = 20 * log10(gain);
+
+  phase(a) = atan(RL/Zeq2); % está mal
   
   a = a + 1;
   
@@ -65,3 +69,12 @@ xlabel("log(f)[Hz]");
 ylabel("Gain[dB]");
 
 print (hf, "gainteo.eps", "-depsc");
+
+f = 1:0.1:8.1; %início, fim e 70 pontos pelo meio
+hf2 = figure();
+plot(f, phase);
+legend("Phase");
+xlabel("log(f)[Hz]");
+ylabel("Phase[rad]");
+
+print (hf2, "phaseteo.eps", "-depsc");
