498f7551b094ebe688cac7bb928f8010618f02b3
Ana Martins <anaisabelmartins@tecnico.ulisboa.pt>
2021-06-08 09:55:46 +0100

    added some more relevant results

--- t5/mat/t5.m
+++ t5/mat/t5.m
@@ -13,22 +13,6 @@ omega = 2 * pi * f;
 ZC1 = 1/(j*omega*C1);
 ZC2 = 1/(j*omega*C2);
 
-%Z1 = R1 + 1/(j * omega * Cin1);
-%Z2 = R3 + 1/(j * omega * Cin2);
-%Z3 = R4 + 1/(j * omega * Cin3);
-
-%Ztemp = 1/(1/Z1 + 1/Z2); %paralelo de Z1 com Z2
-%Zeq = 1/(1/Ztemp + 1/Z3); %impedância ao nível de inv_in
-
-%Vin = 0.01 * exp(j * pi / 2); %seno tem fase de pi/2
-
-%gaincomp = -R2/Zeq; %slide 2 aula 23
-%gain = abs(gaincomp);
-%gaindB = 20 * log10(gain);
-
-%Zi = real(Zeq); %slide 2 aula 23
-%Zo = 0; %estranho :/
-
 Zi = abs(1/(1/R1 + 1/ZC1));
 Zo = abs(1/(1/R2 + 1/ZC2));
 
@@ -52,20 +36,6 @@ a = 1;
 for i = 1:0.1:8.1 %ou seja, 10 pontos por década
   f = power(10, i);
   omega = 2 * pi * f;
-  %Z1 = R1 + 1/(j * omega * Cin1);
-  %Z2 = R3 + 1/(j * omega * Cin2);
-  %Z3 = R4 + 1/(j * omega * Cin3);
-  
-  %Ztemp = 1/(1/Z1 + 1/Z2); %paralelo de Z1 com Z2
-  %Zeq = 1/(1/Ztemp + 1/Z3); %impedância ao nível de inv_in
-
-  %Zeq2 = 1/(1/Z1 + 1/Z2 + 1/Z3) + R2; % usei para tentar calcular a fase mas está mal
-  
-  %gaincomp = -R2/Zeq; %slide 2 aula 23
-  %gain = abs(gaincomp);
-  %gaindB(a) = 20 * log10(gain);
-
-  %phase(a) = atan(RL/Zeq2); % está mal
   
   gaincomp = (R1*C1*j*omega)/(1 + R1*C1*j*omega) * (1 + (R3/R4)) * (1/(1 + R2*C2*j*omega));
   gain = abs(gaincomp);
